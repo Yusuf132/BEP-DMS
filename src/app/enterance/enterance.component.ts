c3b336b5df1dd1f3089375e529143d216d4d6f13
Yousuff Khan <ykhan@corp.broadsoft.com>
2018-03-22 16:35:07 +0530

    First commit

---
+++ src/app/enterance/enterance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'enterance-page',
+  templateUrl: './enterance.component.html',
+  styleUrls: ['./enterance.component.css']
+})
+export class EnteranceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
